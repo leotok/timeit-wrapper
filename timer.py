82995f42433b674f051bc6762207ee412445b6c9
leotok <leo.wajnsztok@gmail.com>
2016-09-19 00:24:04 -0300

    better info

--- timer.py
+++ timer.py
@@ -7,12 +7,14 @@ if __name__ == "__main__":
 		
 	path = os.path.dirname(os.path.abspath(__file__))
 
+	print "loops: ", loops
+	print "folder: ", path
 	for path in os.listdir(path):
 
 		if path != "timer.py" and path != ".DS_Store":
-			print "\n", path, "loops: ", loops
+			print "\n", path
 
  			with open(path) as f:
 				
 				r = f.read()
-				print timeit.timeit(r, number=loops)
+				print "time: ", timeit.timeit(r, number=loops), "secs"
